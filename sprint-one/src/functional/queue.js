d9fc321223ec5ce3ff89c91ab8f209b337e7328b
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-02-05 17:37:36 -0800

    finishing up queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,8 +1,10 @@
 var Queue = function()
 {
+
   var someInstance = {};
-   someInstance.nextUp = 0;
+   someInstance.firstIn = 0;
    someInstance.lastIn = 0;
+
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -10,29 +12,30 @@ var Queue = function()
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-someInstance.nextUp++;
-someInstance.lastIn++;
-storage[someInstance.nextUp] = value;
+storage[someInstance.lastIn++];
+storage[someInstance.lastIn] = value;
   };
-
+debugger;
   someInstance.dequeue = function() {
-    var tempHold = storage[someInstance.nextUp];
-    delete storage[someInstance.nextUp];
-    tempHold = storage[someInstance.nextUp];
+    var tempHold = storage[someInstance.firstIn];
+   delete storage[someInstance.firstIn];
+   someInstance.firstIn++;
 
+   someInstance.lastIn--;
 
     return tempHold;
 
   };
 
   someInstance.size = function() {
- if(someInstance.nextUp <= 0)
+
+ if(someInstance.lastIn <= 0)
  {
    return 0;
  }
  else
  {
-   return someInstance.nextUp;
+   return someInstance.lastIn;
  }
   };
 
