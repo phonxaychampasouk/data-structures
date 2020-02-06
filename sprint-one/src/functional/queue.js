be9739b9ab594becc398302ce2ea72479eb00184
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-02-05 16:27:32 -0800

    Need to finish queue. Have 2 test left.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
-var Queue = function() {
+var Queue = function()
+{
   var someInstance = {};
+   someInstance.nextUp = 0;
+   someInstance.lastIn = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,13 +10,32 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+someInstance.nextUp++;
+someInstance.lastIn++;
+storage[someInstance.nextUp] = value;
   };
 
   someInstance.dequeue = function() {
+    var tempHold = storage[someInstance.nextUp];
+    delete storage[someInstance.nextUp];
+    tempHold = storage[someInstance.nextUp];
+
+
+    return tempHold;
+
   };
 
   someInstance.size = function() {
+ if(someInstance.nextUp <= 0)
+ {
+   return 0;
+ }
+ else
+ {
+   return someInstance.nextUp;
+ }
   };
 
   return someInstance;
 };
+
