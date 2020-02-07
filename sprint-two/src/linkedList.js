d922be217c84431c0572ece250250df1f65d888b
phonxaychampasouk <phonxay.champasouk@outlook.com>
2020-02-07 11:30:20 -0800

    inital commit, completed 1st test

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,18 +3,34 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+
+  //add to end of list
   list.addToTail = function(value) {
+    
+  list.tail = Node(value);
+
+
+
   };
 
+  //remove node that head point to
   list.removeHead = function() {
   };
 
+  //does list contain the target
   list.contains = function(target) {
+    for (var key in list) {
+      if ( key === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
 };
 
+//add node to end of current list
 var Node = function(value) {
   var node = {};
 
