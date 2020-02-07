532efe4bbe1efd18a80d8005c24688abef360840
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-02-06 16:23:30 -0800

    completed Sprint

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.nextUp = 1;
+    this.lastIn = 0;
   }
 
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
+  enqueue(value) {
+    this.lastIn++;
+    this.storage[this.lastIn] = value;
+  }
+
+  dequeue() {
+    if(this.nextUp <= this.lastIn){
+      var temp = this.storage[this.nextUp];
+      delete this.storage[this.nextUp];
+      this.nextUp++;
+      return temp;
+    }
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.lastIn = 0;
   }
 
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
+  push(value) {
+    this.lastIn++;
+    this.storage[this.lastIn] = value;
+  }
+
+  pop() {
+    var temp = this.storage[this.lastIn];
+    delete this.storage[this.lastIn];
+    this.lastIn--;
+    return temp;
+  }
 }
