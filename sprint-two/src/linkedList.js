8e965d536cf83aa911aaba8fd8c948541a7a22de
phonxaychampasouk <phonxay.champasouk@outlook.com>
2020-02-07 20:06:08 -0800

    "completed link list"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,50 +2,41 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
+  list.currentNode = null;
 
   //add to end of list
   list.addToTail = function(value) {
-    var accum = 0;
-if(accum > 0)
-{
-  list.tail = Node(value);
-  list.head = Node(value);
-  accum++;
 
-}
-list.tail = Node(value);
+    list.currentNode = Node(value);
 
-};
+    if (list.head === null) {
+      list.head = list.currentNode;
+    } else {
+      list.head.next = Node(value);
+    }
 
-
-
-
-  //remove node that head point to
-  list.removeHead = function() {
-
-   delete list.head.next;
-
-
-
-
+    list.tail = list.currentNode;
   };
 
 
+  //remove node that head points to
+  list.removeHead = function() {
+    var toBeDeleted = list.head.value;
+    list.head = list.head.next;
+    delete toBeDeleted;
+    return toBeDeleted;
+  };
 
   //does list contain the target
   list.contains = function(target) {
-    for (var key in list) {
-      if ( key === target) {
-        return true;
-      }
+    if ( list.head.value === target || list.tail.value === target ) {
+      return true;
     }
     return false;
   };
-
   return list;
-};
 
+};
 
 
 //add node to end of current list
