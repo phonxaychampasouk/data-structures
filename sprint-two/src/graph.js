0a10a6df8157480cdf1c92e57569c4e2161100de
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-02-09 00:20:25 -0800

    Pass 3 test

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,28 +2,59 @@
 
 // Instantiate a new graph
 var Graph = function() {
+    this.node = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+
+    var addsNode = Nodes(node);
+    this.node = addsNode;
+    
+
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+    if(this.node === undefined)
+    {
+        return false;
+    }
+    return true;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+    delete this.node;
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+   var oneWayNode = Nodes(fromNode);
+    var otherWayNode = Nodes(toNode);
+ if(this.node.oneWayNode === oneWayNode && this.node.otherWayNode === otherWayNode)
+ {
+     return true;
+ }
+ return false;
+
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+    var edges = this.node.edges;
+    for(var i = 0; i<this.node.edges.length; i++)
+    {
+        if
+        (
+        edges[i].push(Nodes(fromNode)) &&
+        edges[i].push(Nodes(toNode))
+        )
+        {
+            return true;
+        }
+    }
 };
-
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
 };
@@ -32,6 +63,12 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 Graph.prototype.forEachNode = function(cb) {
 };
 
+var Nodes =function(value){
+    var node = {};
+    node.value = value;
+    node.edges = [];
+    return node;
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -13,14 +13,26 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
  this.children.push(Tree(value));
+ 
 };
+
 
 treeMethods.contains = function(target) {
 
+ var result = false;
 
- return this.children.includes(target);
+if ( this.value === target) {
+   return true;
+ }
 
-};
+ for (var i = 0; i < this.children.length; i++) {
+   result = this.children[i].contains(target);
+   if (result) {
+     return result;
+   }
+ }
+ return false;
+}
 
 // step one: create root node which, later on in code will check to see if same value is in tree
 
